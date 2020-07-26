d5e09ecd44acabaff31a8451a1312ebe17149240
dlcv <dlcv@live.com.ve>
2020-07-26 16:08:27 -0400

    Environment variable MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -6,10 +6,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // DATABASE
 let urlDB;
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe'
-// } else {
-urlDB = 'mongodb+srv://node-dlcv:CIm06RCF2ufOqwBb@cluster0.x8xwn.mongodb.net/cafe'
-    //}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
